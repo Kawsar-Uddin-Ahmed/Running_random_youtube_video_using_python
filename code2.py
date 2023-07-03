192397fc04c2cb83873e29c18fd9d790d3b3c9c1
Kawsar Uddin Ahmed <52331860+Kawsar-Uddin-Ahmed@users.noreply.github.com>
2023-07-03 23:05:21 +0600

    Update code2.py

--- code2.py
+++ code2.py
@@ -6,12 +6,12 @@ def running():
     for j in range(i):
         c = input()
         with open("Links.txt", "a") as f:
-                print(c, file=f)
+                print(c, file=f) #Here file=f is used so that the input can be written in the created file. 
     with open("Links.txt","r") as m:
         d = m.readlines()
         b = random.choice(d)
         webbrowser.open_new(b)
-    threading.Timer(10,running).start() #It will recall from inside the function means it is recursion
+    threading.Timer(10,running).start() #It will recall from inside the function means it is a recursion
 if __name__ == '__main__':
     running()
 
